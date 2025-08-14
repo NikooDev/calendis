040b106e20cf3bb87f3ca9f8efc9a0f74ab34b5d
Nicolas <nicolas.tual@outlook.fr>
2025-08-14 15:20:16 +0200

    First commit

--- src/components/layout/styles.tsx
+++ src/components/layout/styles.tsx
@@ -2,15 +2,14 @@ import React from 'react';
 
 /**
  * Fix UA styles title h1 :
- * A <h1> was found inside an <article>,
- * <aside>, <nav>, or <section> element that does not have an explicit font size set.
+ * A <h1> was found inside an <article>, <aside>, <nav>, or <section> element that does not have an explicit font size set.
  * The font size for such headings will be changed in this browser soon.
  */
 const Styles = () => {
 	// @ts-ignore
 	return (
 		<style>
-			{`:where(h1) {margin-block: 0.67em;font-size: 2em;}`}
+			{`:where(h1) {margin-block: auto;font-size: 3.75rem;}`}
 		</style>
 	);
 };
