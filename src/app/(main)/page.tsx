3788c95e4bac002c82fc35c47311f1b893c48f8d
Nicolas <nicolas.tual@outlook.fr>
2025-08-14 15:34:27 +0200

    First commit

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Page = () => {
 	return (
 		<section>
-			<h1 className="font-title">coucou</h1>
+			<h1 className="font-title font-black leading-4">coucou</h1>
 		</section>
 	);
 };
