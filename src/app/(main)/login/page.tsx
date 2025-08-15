2718f6a4eb8c138d4c108685b23d4e052655171c
Nicolas <nicolas.tual@outlook.fr>
2025-08-15 04:05:41 +0200

    First commit

--- src/app/(main)/login/page.tsx
+++ src/app/(main)/login/page.tsx
@@ -9,9 +9,7 @@ const Page = () => {
 			<div className="relative px-4 w-full sm:max-w-[500px] z-30 pb-28 lg:pb-0">
 				<div className="flex flex-col items-center justify-center relative">
 					<SlideIn trigger="mount" delay={100} direction="bottom" className="flex flex-col lg:flex-row gap-2 lg:gap-8 mb-10 p-5">
-						<div className="box-shadow rounded-full">
-							<Image src="/static/img/logo.webp" width={100} height={100} className="block rounded-full select-none pointer-events-none" alt="logo" priority/>
-						</div>
+						<Image src="/static/img/logo.webp" width={100} height={100} className="block box-shadow rounded-full select-none pointer-events-none" alt="logo" priority/>
 						<div className="leading-14">
 							<h1 className="font-title text-6xl font-black text-white text-shadow">Calendis</h1>
 							<p className="font-title text-2xl text-shadow font-black ml-0.5 text-white">Connexion</p>
