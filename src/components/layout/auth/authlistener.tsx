ba8d40233523a9532fd229ae3b773c85b08b8bcf
Nicolas <nicolas.tual@outlook.fr>
2025-08-20 05:58:03 +0200

    Update authentification

--- src/components/layout/auth/authlistener.tsx
+++ src/components/layout/auth/authlistener.tsx
@@ -1,19 +1,16 @@
 'use client';
 
 import { useEffect } from 'react';
-import { usePathname } from 'next/navigation';
 import AuthService from '@Calendis/services/auth.service';
 
 const AuthListener = () => {
-	const pathname = usePathname();
-
 	useEffect(() => {
 		AuthService.startAuthListener();
 
 		return () => {
 			AuthService.stopAuthListener();
 		};
-	}, [pathname]);
+	}, []);
 
 	return null;
 };
